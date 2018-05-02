6bce37a0bddfc60ac6ecd87fcb75068cf610b3df
daanish <daanish.husain@gmail.com>
2018-05-02 15:48:26 -0700

    code cleanup

--- client/src/screens/articles/index.js
+++ client/src/screens/articles/index.js
@@ -1,7 +1,1 @@
-export * from './articles1';
-export * from './articles2';
-export * from './articles3';
-export * from './articles4';
-export * from './articlesRec';
-export * from './blogposts';
 export * from './article';
--- client/src/screens/login/index.js
+++ client/src/screens/login/index.js
@@ -1,4 +1,2 @@
 export * from './login1';
-export * from './login2';
 export * from './signUp';
-export * from './passwordRecovery';
